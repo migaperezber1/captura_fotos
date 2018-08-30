9620372dcd4032e2f10a49df4b9d180cafcf15b6
migaperezber1 <migaperezber@hotmail.com>
2018-08-30 12:02:02 -0500

    Update imagenes.py

--- imagenes.py
+++ imagenes.py
@@ -1,4 +1,5 @@
 #import OpenCV module
+
 import cv2
 #import os module for reading training data directories and paths
 import os, sys
@@ -7,27 +8,32 @@ import os, sys
 import numpy as np
 
 from PIL import Image, ImageDraw, ImageFont
-video_capture1 = cv2.VideoCapture(0)
-video_capture2 = cv2.VideoCapture(1)
-video_capture3 = cv2.VideoCapture(2)
-image1 = cv2.imread('1.jpg',1)
-image2 = cv2.imread('2.jpg',1)
+video_capture1 = cv2.VideoCapture(0) #selfie
+video_capture2 = cv2.VideoCapture(1) #documento
 fondo = cv2.imread('davivienda.jpg',1)
-re_im1= cv2.resize (image1, (300, 300))
+bot=cv2.imread('boton.jpg',1)
+boton=cv2.resize(bot,(100,60))
 re_fondo= cv2.resize(fondo, (700, 630))
 fondo_copy= cv2.resize(fondo, (700, 630))
 #face_recort = gray1[y:y+h, x:x+w]
 def button_actions(event,x,y,flags,param):
-	if (event == cv2.EVENT_LBUTTONDOWN and x>630 and x<690 and y>10 and y<50):
+	if (event == cv2.EVENT_LBUTTONDOWN and x>430 and x<530 and y>400 and y<455): #captura para documento
 		ret, foto1 =video_capture2.read()
 		re_foto1= cv2.resize (foto1, (300, 300))
 		re_fondo[320:620, 5:305]=re_foto1
-	if (event == cv2.EVENT_LBUTTONDOWN and x>630 and x<690 and y>10 and y<50):
-		ret, foto2 =video_capture3.read()
-		re_foto2= cv2.resize (foto1, (300, 300))
+
+	if (event == cv2.EVENT_LBUTTONDOWN and x>430 and x<530 and y>470 and y<520): #captura para camara
+		ret, foto2 =video_capture1.read()
+		re_foto2= cv2.resize (foto2, (300, 300))
+		faces, eyes=detect_face(re_foto2)
+		if (faces is not None):
+			draw_rectangle(re_foto2, faces[0])
+		if (eyes is not None):
+			for eye in eyes:
+				draw_rectangle(re_foto2, eye)
 		re_fondo[5:305, 5:305]=re_foto2
 	
-	elif (event == cv2.EVENT_LBUTTONDOWN and x>630 and x<690 and y>130 and y<170):
+	elif (event == cv2.EVENT_LBUTTONDOWN and x>430 and x<530 and y>540 and y<590):
 		re_fondo[5:305,5:305]=fondo_copy[5:305,5:305]
 		re_fondo[320:620, 5:305]=fondo_copy[320:620, 5:305]
 		
@@ -35,54 +41,55 @@ def button_actions(event,x,y,flags,param):
 
 def detect_face(img):
     #convert the test image to gray image as opencv face detector expects gray images
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     
     #load OpenCV face detector, I am using LBP which is fast
     #there is also a more accurate but slow Haar classifier
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+	
     #let's detect multiscale (some images may be closer to camera than others) images
     #result is a list of faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5);
-    
-    #if no faces are detected then return original img
-    if (len(faces) == 0):
-        return None
-    #num_face=0
-    #for (x,y,w,h) in faces:
-        #extract the face area
-     #   (x, y, w, h) = faces[num_face]
-      #  num_face+=
-    #return only the face part of the image
-    #return gray[y:y+w, x:x+h], faces[num_face]
-    return faces
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4);
+	eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5);
+	if(len(faces)==0):
+		return None, None
+	elif (len(eyes)==0):
+		return faces, None	
+	return faces,eyes
 
-def draw_rectangle(img, rect):
+def draw_rectangle(img, rect,):
     (x, y, w, h) = rect
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 100), 2)
 		
 img=re_fondo
 cv2.namedWindow('img')
 cv2.setMouseCallback('img',button_actions)
 
 #botones y textos
-cv2.rectangle(re_fondo, (630,10), (690, 50), (20, 0, 150), -2)
-cv2.putText(re_fondo, "Documento", (631,30), cv2.FONT_HERSHEY_DUPLEX, 0.33, (0, 0, 0), 0)
 
-cv2.rectangle(re_fondo, (630,70), (690, 110), (20, 0, 150), -2)
-cv2.putText(re_fondo, "Foto", (640,90), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 0, 0), 0)
+re_fondo[400:460, 430:530]=boton
+cv2.putText(re_fondo, "Documento", (435,432), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 0)
 
-cv2.rectangle(re_fondo, (630,130), (690, 170), (20, 0, 150), -2)
-cv2.putText(re_fondo, "limpiar", (631,150), cv2.FONT_HERSHEY_DUPLEX, 0.53, (0, 0, 0), 0)
+re_fondo[468:528, 430:530]=boton
+cv2.putText(re_fondo, "Foto", (445,505), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 0, 0), 0)
+
+re_fondo[538:598, 430:530]=boton
+cv2.putText(re_fondo, "limpiar", (440,570), cv2.FONT_HERSHEY_DUPLEX, 0.53, (0, 0, 0), 0)
 
 
 while True:
 	ret2, foto1= video_capture2.read()
-	ret, test_img1 = video_capture1.read()
-	re_im2= cv2.resize (test_img1, (300, 300))
-	re_fondo[5:305,5:305]=re_im1
-	re_fondo[5:305, 315:615]=re_im2
+	ret, live = video_capture1.read()
+	re_live= cv2.resize (live, (300, 300))
+	faces, eyes = detect_face(re_live)
+	if (faces is not None):
+		draw_rectangle(re_live, faces[0])
+	if (eyes is not None):
+			for eye in eyes:
+				draw_rectangle(re_live, eye)
+	re_fondo[5:305, 315:615]=re_live
 	
 	cv2.imshow('img',re_fondo )  
 	if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -91,3 +98,5 @@ while True:
 # When everything is done, release the capture
 video_capture1.release()
 cv2.destroyAllWindows()	
+
+#Miguel Perez - migaperezber@hotmail.com
